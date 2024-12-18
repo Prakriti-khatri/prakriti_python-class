3a39d86760fced3d47f7db7c9cb117b479c4ef19
prakriti khatri <prakritikhatri746@gmail.com>
2024-12-18 16:45:59 +0545

    basic code

--- pratice.py
+++ pratice.py
@@ -57,3 +57,25 @@ elif user_choice=="3":
             empty_list.append("apple")
             empty_list.append("banana")
             print(empty_list)
+
+
+            empty_list=[]  
+empty_list.append("apple")
+empty_list.append("banana")
+empty_list.append("kiwi")
+empty_list.append("papaya")
+for index, fruit in enumerate(empty_list):
+    print(f"item position:{index}and value:{fruit}")
+
+fruits=("apple","banana","cherry","straberry","raspberry")
+print(fruits[1])
+(green ,yellow ,*red)=fruits
+print(green,yellow,red)
+
+fruits={"apple","banana","cherry","straberry"}
+for item in fruits:
+    print(item)
+    fruits.add("apple")
+    print(fruits)
+fruits.remove("banana")
+print(fruits)
--- start.py
+++ start.py
@@ -1,20 +1,76 @@
-empty_list=[]  
-empty_list.append("apple")
-empty_list.append("banana")
-empty_list.append("kiwi")
-empty_list.append("papaya")
-for index, fruit in enumerate(empty_list):
-    print(f"item position:{index}and value:{fruit}")
+batteries=[50,30,4,45,12,18,30]
+minimum_battery_power=20
+usable_battery_power=0
+usable_battery_count=0
+for battery in batteries:
+    if battery>minimum_battery_power:
+        usable_battery_power+=battery
+        usable_battery_count=usable_battery_count+1
+        print(f"there are{usable_battery_count}batteries which can be used to generate{usable_battery_power}")
 
-fruits=("apple","banana","cherry","straberry","raspberry")
-print(fruits[1])
-(green ,yellow ,*red)=fruits
-print(green,yellow,red)
+#alien message game
+alien_message="uoy era woh namuh ih"
+print(f"""
+alien message={alien_message}
+now, human message={alien_message[::-1]} """)#reverse string
 
-fruits={"apple","banana","cherry","straberry"}
-for item in fruits:
-    print(item)
-    fruits.add("apple")
-    print(fruits)
-fruits.remove("banana")
-print(fruits)
+available_food=[
+    "apple",
+    "banana",
+    "chocolate",
+   "mango",
+   "kiwi",
+   "water",
+   "breads",
+   "cupcake",
+   "pizza",
+   "momo",
+]
+available_crews=3
+each_crew_food=len(available_food)//available_crews
+remaining_food_count=len(available_food)%available_crews
+print(f"each_crew_get{each_crew_food}food and remaining food count={remaining_food_count}")
+
+#functions
+
+
+def setup_mission():
+    print("setting up for the mission....")
+    available_food=[
+    "apple",
+    "banana",
+    "chocolate",
+   "mango",
+   "kiwi",
+   "water",
+   "breads",
+   "cupcake",
+   "pizza",
+   "momo",
+]
+    available_crews=int(input("enter available crew"))
+    print("setup completed....")
+    return available_crews, available_food
+ 
+
+def alien_attack_game():
+
+    print("welcome to alien attack game")
+    print("starting mission.....")
+    
+    crews_number,foods=setup_mission()
+    print(f"you have {crews_number}astronuts and food available={foods}")
+   
+    print("WELCOME TO THE MARS!!!")
+    print("your battery is dead please charge the battery")
+
+
+   
+   
+   
+    print("mission completed")
+
+alien_attack_game()
+
+
+
