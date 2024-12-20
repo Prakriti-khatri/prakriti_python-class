7b166cf8fe4107a5c823b2977bfa3ef53e1c5f71
prakriti khatri <prakritikhatri746@gmail.com>
2024-12-20 16:43:34 +0545

    alien game assignment

--- assignment.py
+++ assignment.py
@@ -1,65 +1,17 @@
-x = float(input("Enter a number: "))
-result = (((2 * x) + 10) / 2) - x
-
-print("The result is:", result)
-
-input("enter the number")
-
+my_list=['1','2','3','a','b','c']
+my_list.append('d')
+my_list.remove('2')
+print(len(my_list))
+print(my_list)
 
 
-
-length = float(input("enter the length of the rectangle"))
-width=float(input("enter the width of the rectangle"))
-area=length*width
-perimeter=2 *(length+width)
-print(f"rectangle area{area:2f}")
-print(f"rectangle perimater{perimeter:2f}")
-
-
-radius= float(input("enter the radius of the circle "))
-area = 22/7* radius**2
-circumference = 2 * 22/7 * radius
-print(f"circle area{area}")
-print(f"circle circumference{circumference}")
+my_tuple=('10','20','30','40','50')
+print(my_tuple[2])
+p_1=list(my_tuple)
+p_1[3]=35
+b_1=tuple(p_1)
+print(b_1)
 
 
-#Currency (using hardcoded exchange rates)
-exchange_rates = {
-    "USD_TO_INR": 82.5,
-    "USD_TO_EUR": 0.91,
-    "INR_TO_USD": 0.012,
-    "INR_TO_EUR": 0.011,
-}
 
-amount = float(input("Enter the amount: "))
-conversion_type = input("Enter the conversion (e.g., USD_TO_INR, INR_TO_USD): ").upper()
-if conversion_type in exchange_rates:
-    converted_amount = amount * exchange_rates[conversion_type]
-    print(f"{amount} {conversion_type.split('TO')[0]} is equal to {converted_amount:.2f} {conversion_type.split('TO')[1]}")
-else:
-    print("Invalid conversion type. Please try again.")
-
-
-        
-#to calculate simple interest 
-principal = float(input("Enter the principal amount: "))
-rate = float(input("Enter the annual rate of interest (in %): "))
-time = float(input("Enter the time (in years): "))
-simple_interest = (principal*rate*time)/100
-print(f"The Simple Interest is: {simple_interest:.2f}")
-
-
-#Calculate the area and perimeter of shapes (triangle))
-a = float(input("Enter the lenght of one side:"))
-b = float(input("Enter the base of  triangle:"))
-c = float(input("Enter the heigth of triangle:"))
-Area=1/2*b*c
-Perimeter=a+b+c
-print(f"The Area is:{Area:.2f}")
-print(f"The Perimeter is: {Perimeter:.2f}" )
-
-
-#check wheather the number is a multiple of another number.
-num1=float(input("enter a first number"))
-num2=float(input("enter a second number"))
-
+print("surrender")[::-1]
